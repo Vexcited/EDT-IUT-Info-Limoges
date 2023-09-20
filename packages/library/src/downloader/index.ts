faa3a83808d75de9d5dfeadf63ce9665ce18e7e9
Mikkel RINGAUD <mikkel@milescode.dev>
2023-09-20 22:01:18 +0200

    fix(downloader): throw if the year is incorrect

--- packages/library/src/downloader/index.ts
+++ packages/library/src/downloader/index.ts
@@ -2,6 +2,10 @@ import { FTP_ENDPOINT_URL, YEARS } from "./constants.js";
 import { TimetableEntry } from "./entry.js";
 
 export const getTimetableEntries = async (from: YEARS): Promise<TimetableEntry[]> => {
+  if (!Object.values(YEARS).includes(from)) {
+    throw new Error(`"from" parameter is invalid, given year: '${from}'. Should be one of: ${Object.values(YEARS).join(", ")}`);
+  }
+
   const response = await fetch(`${FTP_ENDPOINT_URL}/${from}`);
   const html = await response.text();
 
