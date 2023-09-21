a719922c4f68f5b5aacdb11834a3ffdf63f2a337
Mikkel RINGAUD <mikkel@milescode.dev>
2023-09-21 23:05:23 +0200

    feat(bounds): add start_y_offset

--- packages/library/src/parser/bounds.ts
+++ packages/library/src/parser/bounds.ts
@@ -1,4 +1,4 @@
-import type { Fill, Page, Text } from "../converter/pdfparser.js";
+import type { Fill, Page, Text } from "../converter/types";
 
 export interface FillBounds {
   start_x: number;
@@ -27,7 +27,7 @@ export const getFillBounds = (fill: Fill): FillBounds => ({
 export const getTextsInFillBounds = (page: Page, bounds: FillBounds, end_y_offset = 0, start_y_offset = 0): Text[] => {
   return page.Texts.filter(text => {
     const x_in_bounds = text.x >= bounds.start_x && text.x <= bounds.end_x;
-    const y_in_bounds = text.y >= bounds.start_y && text.y <= (bounds.end_y + end_y_offset);
+    const y_in_bounds = text.y >= (bounds.start_y + start_y_offset) && text.y <= (bounds.end_y + end_y_offset);
 
     return x_in_bounds && y_in_bounds;
   });
