cae5cc612dff91553dacc8698bb2ccec17858ac4
Mikkel RINGAUD <mikkel@milescode.dev>
2023-09-21 08:50:28 +0200

    chore: styling

--- packages/library/jest.config.ts
+++ packages/library/jest.config.ts
@@ -1,22 +1,15 @@
-import type { JestConfigWithTsJest } from 'ts-jest'
+import type { JestConfigWithTsJest } from "ts-jest";
 
-const jestConfig: JestConfigWithTsJest = {
-  // [...]
-  preset: 'ts-jest/presets/default-esm', // or other ESM presets
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
+const config: JestConfigWithTsJest = {
+  preset: "ts-jest/presets/default-esm",
+  moduleNameMapper: { "^(\\.{1,2}/.*)\\.js$": "$1" },
+
   transform: {
-    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        useESM: true,
-      },
-    ],
-  },
-}
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      { useESM: true }
+    ]
+  }
+};
 
-export default jestConfig
-
+export default config;
