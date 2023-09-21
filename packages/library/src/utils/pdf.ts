bdde1399a9fe82872ebaf9bb10622a2ecc1e1135
Mikkel RINGAUD <mikkel@milescode.dev>
2023-09-21 23:38:22 +0200

    fix(pdf): destroy class and use correct paths

--- packages/library/src/utils/pdf.ts
+++ packages/library/src/utils/pdf.ts
@@ -1,16 +1,16 @@
-import type { Output } from "../converter/types";
+import type { Output } from "../converter";
 import PDFParser from "../converter";
 
-// export const getRawPDF = async (pdf_buffer: ArrayBuffer): Promise<Output> => {
-//   const parser = new PDFParser();
-//   return parser.parseBuffer(new Uint8Array(pdf_buffer));
-// }
-
-export const getRawPDF = async (pdf_buffer: ArrayBuffer): Promise<Output> => new Promise<Output>((resolve, reject) => {
+export const getRawPDF = async (pdf_buffer: ArrayBuffer): Promise<Output> => {
   const parser = new PDFParser();
 
-  parser.on("pdfParser_dataError", reject);
-  parser.on("pdfParser_dataReady", resolve);
+  const data = await new Promise<Output>((resolve, reject) => {
+    parser.on("pdfParser_dataError", reject);
+    parser.on("pdfParser_dataReady", resolve);
 
-  parser.parseBuffer(pdf_buffer);
-});
+    parser.parseBuffer(pdf_buffer);
+  })
+  
+  parser.destroy();
+  return data;
+};
