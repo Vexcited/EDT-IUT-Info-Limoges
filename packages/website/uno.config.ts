d4406f97e341ad01dd868d0f0ca087bc43b783ef
Mikkel RINGAUD <mikkel@milescode.dev>
2023-09-24 17:18:00 +0200

    feat(web): add theming

--- packages/website/uno.config.ts
+++ packages/website/uno.config.ts
@@ -3,5 +3,14 @@ import { defineConfig, presetUno } from 'unocss'
 export default defineConfig({
   presets: [
     presetUno()
-  ]
+  ],
+
+  theme: {
+    colors: {
+      white: "var(--white)",
+      gray: "var(--gray)",
+      "subgray-1": "var(--subgray-1)",
+      "subgray-2": "var(--subgray-2)",
+    }
+  }
 });
