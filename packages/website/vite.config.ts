1ef3e979ce214863f3b0c2dd3c470113f7ab592c
Mikkel RINGAUD <mikkel@milescode.dev>
2023-09-25 07:38:15 +0200

    feat(web): add PWA

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,7 +1,7 @@
 import unocss from 'unocss/vite'
 import solid from "solid-start/vite";
 import icons from 'unplugin-icons/vite';
-
+import { VitePWA as pwa } from "vite-plugin-pwa";
 
 import { defineConfig } from "vite";
 
@@ -18,6 +18,48 @@ export default defineConfig({
     }),
 
     unocss(),
-    icons({ compiler: "solid" })
+    icons({ compiler: "solid" }),
+
+    pwa({
+      base: "/",
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+
+      workbox: {
+        globPatterns: [
+          "**/*.{js,css,html,svg,png,woff,woff2}"
+        ]
+      },
+
+      manifest: {
+        name: "EDT - IUT Informatique de Limoges",
+        short_name: "EDT - IUT",
+        description: "Une PWA simple et fonctionnelle hors-ligne à utiliser pour visualiser son emploi du temps n'importe quand de façon instantané.",
+
+        categories: [
+          "productivity"
+        ],
+
+        icons: [
+          {
+            src: "/icons/default.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/icons/default.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ],
+
+        start_url: "/",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait"
+      }
+    })
   ]
 });
