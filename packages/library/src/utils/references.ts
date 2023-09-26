1218f1bbd1ac1fb006284f08fb117f8511b53255
Mikkel RINGAUD <mikkel@milescode.dev>
2023-09-26 09:10:56 +0200

    fix(pkg): shorter references

--- packages/library/src/utils/references.ts
+++ packages/library/src/utils/references.ts
@@ -12,15 +12,15 @@ export const BUT_INFO_REF = {
 
   "R1.01": "Initiation au développement",
   "R1.02": "Développement d'interfaces web",
-  "R1.03": "Introduction à l'architecture des ordinateurs",
-  "R1.04": "Introduction aux systèmes d'exploitation et à leur fonctionnement",
-  "R1.05": "Introduction aux bases de données et SQL",
+  "R1.03": "Architecture des ordinateurs",
+  "R1.04": "Systèmes d'exploitation",
+  "R1.05": "Bases de données et SQL",
   "R1.06": "Mathématiques discrètes",
   "R1.07": "Outils mathématiques fondamentaux",
-  "R1.08": "Introduction à la gestion des organisations",
-  "R1.09": "Introduction à l'économie durable et numérique",
+  "R1.08": "Organisations",
+  "R1.09": "Économie durable et numérique",
   "R1.10": "Anglais",
-  "R1.11": "Bases de la communication",
+  "R1.11": "Communication",
   "R1.12": "Projet professionnel et personnel",
 
   // Semestre 2, commun.
