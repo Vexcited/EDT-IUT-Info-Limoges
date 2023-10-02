789a63e447a85f9f1def965f2adbebc4d4d627ed
Mikkel RINGAUD <mikkel@milescode.dev>
2023-10-02 22:12:31 +0200

    fix(web): manual rewrite for Vercel

--- packages/website/src/routes/api/pdf/[year]/[file_name].ts
+++ packages/website/src/routes/api/pdf/[year]/[file_name].ts
@@ -1,5 +1,11 @@
 import { type APIEvent } from "solid-start";
 
+/**
+ * This only works in development, for some reasons it breaks
+ * when running on Vercel.
+ * 
+ * So we're using a proxy inside our vercel.json configuration instead, overwriting this.
+ */
 export const GET = async ({ params }: APIEvent): Promise<Response> => {
   const url = "http://edt-iut-info.unilim.fr/edt/" + params.year + "/" + params.file_name;
   const response = await fetch(url);
