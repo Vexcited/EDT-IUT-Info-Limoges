c238b7c220ae267a572ca3ed71488630679ba04d
Mikkel RINGAUD <mikkel@milescode.dev>
2023-11-26 17:22:43 +0100

    fix(lesson): explicit check previous `end_date` with current `start_date`

--- packages/website/src/utils/lessons.ts
+++ packages/website/src/utils/lessons.ts
@@ -67,7 +67,7 @@ export const lessonsForSubGroup = (timetable: Omit<ITimetable, "last_update">, p
       }
 
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      if (previousLesson && (previousLesson.content as any).type === (lesson.content as any).type && previousLesson.type === lesson.type && previousLesson.content.room === lesson.content.room && previousLesson.content.teacher === lesson.content.teacher) {
+      if (previousLesson && (previousLesson.content as any).type === (lesson.content as any).type && previousLesson.type === lesson.type && previousLesson.content.room === lesson.content.room && previousLesson.content.teacher === lesson.content.teacher && previousLesson.end_date === lesson.start_date) {
         previousLesson.end_date = lesson.end_date;
       }
       else {
