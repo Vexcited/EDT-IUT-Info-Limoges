d6eade8141ca6b7f17c07ff2bf198d8983065dd1
Mikkel RINGAUD <mikkel@milescode.dev>
2023-12-06 08:01:07 +0100

    refactor(web): global Modal component

--- packages/website/src/components/modals/Settings.tsx
+++ packages/website/src/components/modals/Settings.tsx
@@ -1,7 +1,6 @@
 import type { Component, Setter } from "solid-js";
-import { Dialog, Switch } from "@kobalte/core";
-
-import MdiClose from '~icons/mdi/close'
+import { Modal } from "~/components/modals";
+import { Switch } from "@kobalte/core";
 
 import { DEFAULT_USER_CUSTOMIZATION, getUserCustomizationKey, preferences, setMainGroup, setSubGroup, setUserCustomization } from "~/stores/preferences";
 import { resetAppCache } from "~/stores/timetables";
@@ -27,182 +26,168 @@ export const SettingsModal: Component<{ open: boolean, setOpen: Setter<boolean> 
   const userPrimaryColor = () => preferences.customization.primary_color ?? DEFAULT_USER_CUSTOMIZATION.primary_color;
 
   return (
-    <Dialog.Root open={props.open} onOpenChange={(open) => props.setOpen(open)} modal={true}>
-      <Dialog.Portal>
-        <Dialog.Overlay class="fixed inset-0 z-50 bg-[rgba(21,21,21,.6)] backdrop-blur-md" />
+    <Modal title="Paramètres"
+      modal={true}
+      open={props.open}
+      setOpen={props.setOpen}
+    >
+      <div class="p-4 flex flex-col gap-6">
+        <section>
+          <h3 class="text-[rgb(240,240,240)] text-[18px] mb-2">
+            Groupe principal
+          </h3>
+          <div class="flex">
+            <button type="button"
+              disabled={preferences.main_group <= 1}
+              class="text-center border-2 border-red text-[rgb(220,220,220)] disabled:(bg-red text-transparent) px-4 py-1 border-r-none rounded-l-lg"
+              onClick={() => {
+                let value = preferences.main_group - 1;
+                if (value === 6) value--;
+                if (value < 1) return;
 
-        <div class="fixed inset-0 z-50 flex items-center justify-center p-4 overflow-auto">
-          <Dialog.Content class="bg-[rgb(32,32,32)] rounded-lg max-w-[450px] w-full my-auto">
-            <div class="relative flex item justify-between items-center mb-2 border-b border-b-[rgb(90,90,90)] p-4 text-[rgb(220,220,220)]">
-              <Dialog.Title class="text-[20px] font-medium">
-                Paramètres
-              </Dialog.Title>
-              <Dialog.CloseButton class="flex items-center justify-center p-1">
-                <MdiClose class="text-lg" />
-              </Dialog.CloseButton>
-            </div>
+                setMainGroup(value);
+              }}
+            >
+              -1
+            </button>
+            <p
+              aria-label="Groupe principal"
+              class="w-full text-center text-[rgb(240,240,240)] bg-red px-3 py-1 font-medium text-lg"
+            >
+              G{preferences.main_group}
+            </p>
+            <button type="button"
+              disabled={preferences.main_group >= 8}
+              class="text-center border-2 border-red text-[rgb(220,220,220)] disabled:(bg-red text-transparent) px-4 py-1 border-l-none rounded-r-lg"
+              onClick={() => {
+                let value = preferences.main_group + 1;
+                if (value === 6) value++;
+                if (value > 8) return;
 
-            <div class="p-4 flex flex-col gap-6">
-              <section>
-                <h3 class="text-[rgb(240,240,240)] text-[18px] mb-2">
-                  Groupe principal
-                </h3>
-                <div class="flex">
-                  <button type="button"
-                    disabled={preferences.main_group <= 1}
-                    class="text-center border-2 border-red text-[rgb(220,220,220)] disabled:(bg-red text-transparent) px-4 py-1 border-r-none rounded-l-lg"
-                    onClick={() => {
-                      let value = preferences.main_group - 1;
-                      if (value === 6) value--;
-                      if (value < 1) return;
+                setMainGroup(value);
+              }}
+            >
+              +1
+            </button>
+          </div>
 
-                      setMainGroup(value);
-                    }}
-                  >
-                    -1
-                  </button>
-                  <p
-                    aria-label="Groupe principal"
-                    class="w-full text-center text-[rgb(240,240,240)] bg-red px-3 py-1 font-medium text-lg"
-                  >
-                    G{preferences.main_group}
-                  </p>
-                  <button type="button"
-                    disabled={preferences.main_group >= 8}
-                    class="text-center border-2 border-red text-[rgb(220,220,220)] disabled:(bg-red text-transparent) px-4 py-1 border-l-none rounded-r-lg"
-                    onClick={() => {
-                      let value = preferences.main_group + 1;
-                      if (value === 6) value++;
-                      if (value > 8) return;
+          <p class="text-[rgb(190,190,190)] text-xs mt-1.5">
+            Ajustez le groupe dans lequel vous êtes (ex.: G1, G2, …) <br />
+            C'est avec le groupe qu'est déterminée l'année.
+          </p>
+        </section>
 
-                      setMainGroup(value);
-                    }}
-                  >
-                    +1
-                  </button>
-                </div>
+        <section>
+          <h3 class="text-[rgb(240,240,240)] text-[18px] mb-2">
+            Sous-groupe
+          </h3>
 
-                <p class="text-[rgb(190,190,190)] text-xs mt-1.5">
-                  Ajustez le groupe dans lequel vous êtes (ex.: G1, G2, …) <br />
-                  C'est avec le groupe qu'est déterminée l'année.
-                </p>
-              </section>
+          <div class="flex gap-2">
+            <SelectionButton
+              content="A"
+              active={preferences.sub_group === 0}
+              onClick={() => setSubGroup(0)}
+            />
+            <SelectionButton
+              content="B"
+              active={preferences.sub_group === 1}
+              onClick={() => setSubGroup(1)}
+            />
+          </div>
 
-              <section>
-                <h3 class="text-[rgb(240,240,240)] text-[18px] mb-2">
-                  Sous-groupe
-                </h3>
+          <p class="text-[rgb(190,190,190)] text-xs mt-1.5">
+            Ajustez le sous-groupe dans lequel vous êtes (ex.: G{preferences.main_group}A ou G{preferences.main_group}B)
+          </p>
+        </section>
 
-                <div class="flex gap-2">
-                  <SelectionButton
-                    content="A"
-                    active={preferences.sub_group === 0}
-                    onClick={() => setSubGroup(0)}
-                  />
-                  <SelectionButton
-                    content="B"
-                    active={preferences.sub_group === 1}
-                    onClick={() => setSubGroup(1)}
-                  />
-                </div>
+        <section>
+          <div class="flex items-center gap-2 mb-2">
+            <h3 class="text-[rgb(240,240,240)] text-[18px] flex">
+              Couleur de l'interface
+            </h3>
+            <div
+              class="w-4 h-4 rounded-full"
+              style={{ "background-color": `rgb(${userPrimaryColor()})` }}
+            ></div>
+          </div>
+          <input
+            type="text"
+            class="px-3 py-1 w-full rounded-lg outline-none focus:outline-red"
+            value={userPrimaryColor()}
+            onInput={(e) => {
+              setUserCustomization({
+                ...preferences.customization,
+                primary_color: e.currentTarget.value,
+              });
+            }}
+          />
+          <p class="text-[rgb(190,190,190)] text-xs mt-1.5">
+            Attention : la couleur doit être définie dans le format "r,g,b".
+          </p>
+        </section>
 
-                <p class="text-[rgb(190,190,190)] text-xs mt-1.5">
-                  Ajustez le sous-groupe dans lequel vous êtes (ex.: G{preferences.main_group}A ou G{preferences.main_group}B)
-                </p>
-              </section>
+        <section>
+          <h3 class="text-[rgb(240,240,240)] text-[18px]">
+            Vue de l'EDT 
+          </h3>
 
-              <section>
-                <div class="flex items-center gap-2 mb-2">
-                  <h3 class="text-[rgb(240,240,240)] text-[18px] flex">
-                    Couleur de l'interface
-                  </h3>
-                  <div
-                    class="w-4 h-4 rounded-full"
-                    style={{ "background-color": `rgb(${userPrimaryColor()})` }}
-                  ></div>
-                </div>
-                <input
-                  type="text"
-                  class="px-3 py-1 w-full rounded-lg outline-none focus:outline-red"
-                  value={userPrimaryColor()}
-                  onInput={(e) => {
-                    setUserCustomization({
-                      ...preferences.customization,
-                      primary_color: e.currentTarget.value,
-                    });
-                  }}
-                />
-                <p class="text-[rgb(190,190,190)] text-xs mt-1.5">
-                  Attention : la couleur doit être définie dans le format "r,g,b".
-                </p>
-              </section>
+          <Switch.Root
+            class="flex items-center justify-between mt-2"
+            checked={getUserCustomizationKey("use_fixed_height")}
+            onChange={value => setUserCustomization({
+              ...preferences.customization,
+              use_fixed_height: value
+            })}
+          >
+            <Switch.Label class="mr-1.5 text-[rgb(220,220,220)] select-none">
+              Alignement sur les heures
+            </Switch.Label>
+            <Switch.Input />
+            <Switch.Control class="inline-flex items-center h-6 w-11 rounded-full px-.5 bg-[rgb(48,48,48)] ui-checked:bg-red transition-colors">
+              <Switch.Thumb class="h-5 w-5 rounded-full bg-white ui-checked:(transform translate-x-[calc(100%-1px)]) transition-transform" />
+            </Switch.Control>
+          </Switch.Root>
 
-              <section>
-                <h3 class="text-[rgb(240,240,240)] text-[18px]">
-                  Vue de l'EDT 
-                </h3>
+          <p class="text-[rgb(190,190,190)] text-xs mt-1.5">
+            Le positionnement des cours sur l'emploi du temps sera aligné sur les heures.
+          </p>
 
-                <Switch.Root
-                  class="flex items-center justify-between mt-2"
-                  checked={getUserCustomizationKey("use_fixed_height")}
-                  onChange={value => setUserCustomization({
-                    ...preferences.customization,
-                    use_fixed_height: value
-                  })}
-                >
-                  <Switch.Label class="mr-1.5 text-[rgb(220,220,220)] select-none">
-                    Alignement sur les heures
-                  </Switch.Label>
-                  <Switch.Input />
-                  <Switch.Control class="inline-flex items-center h-6 w-11 rounded-full px-.5 bg-[rgb(48,48,48)] ui-checked:bg-red transition-colors">
-                    <Switch.Thumb class="h-5 w-5 rounded-full bg-white ui-checked:(transform translate-x-[calc(100%-1px)]) transition-transform" />
-                  </Switch.Control>
-                </Switch.Root>
+          {/* <Switch.Root class="flex items-center justify-between mt-4">
+            <Switch.Label class="mr-1.5 text-[rgb(220,220,220)] select-none">
+              Vue semaine (PC uniquement)
+            </Switch.Label>
+            <Switch.Input />
+            <Switch.Control class="inline-flex items-center h-6 w-11 rounded-full px-.5 bg-[rgb(48,48,48)] ui-checked:bg-red transition-colors">
+              <Switch.Thumb class="h-5 w-5 rounded-full bg-white ui-checked:(transform translate-x-[calc(100%-1px)]) transition-transform" />
+            </Switch.Control>
+          </Switch.Root>
 
-                <p class="text-[rgb(190,190,190)] text-xs mt-1.5">
-                  Le positionnement des cours sur l'emploi du temps sera aligné sur les heures.
-                </p>
+          <p class="text-[rgb(190,190,190)] text-xs mt-1.5">
+            Désactive la navigation en "slide" et affiche l'emploi du temps sur une semaine entière.
+          </p> */}
+        </section>
+      </div>
 
-                {/* <Switch.Root class="flex items-center justify-between mt-4">
-                  <Switch.Label class="mr-1.5 text-[rgb(220,220,220)] select-none">
-                    Vue semaine (PC uniquement)
-                  </Switch.Label>
-                  <Switch.Input />
-                  <Switch.Control class="inline-flex items-center h-6 w-11 rounded-full px-.5 bg-[rgb(48,48,48)] ui-checked:bg-red transition-colors">
-                    <Switch.Thumb class="h-5 w-5 rounded-full bg-white ui-checked:(transform translate-x-[calc(100%-1px)]) transition-transform" />
-                  </Switch.Control>
-                </Switch.Root>
+      <div class="h-[1px] w-full border-b border-b-[rgb(90,90,90)]" />
 
-                <p class="text-[rgb(190,190,190)] text-xs mt-1.5">
-                  Désactive la navigation en "slide" et affiche l'emploi du temps sur une semaine entière.
-                </p> */}
-              </section>
-            </div>
+      <section class="p-4">
+        <h3 class="text-[rgb(240,240,240)] text-[18px] mb-2">
+          Zone dangereuse
+        </h3>
 
-            <div class="h-[1px] w-full border-b border-b-[rgb(90,90,90)]" />
-
-            <section class="p-4">
-              <h3 class="text-[rgb(240,240,240)] text-[18px] mb-2">
-                Zone dangereuse
-              </h3>
-
-              <button class="w-full text-center border border-red text-red hover:bg-red/10 active:bg-red/20 transition px-3 py-1 rounded-lg"
-                type="button"
-                onClick={async () => {
-                  await resetAppCache();
-                  window.location.reload();
-                }}
-              >
-                Effacer le cache
-              </button>
-              <p class="text-[rgb(190,190,190)] text-xs mt-1.5">
-                Supprime le cache stocké par l'application dans le navigateur.
-              </p>
-            </section>
-
-          </Dialog.Content>
-        </div>
-      </Dialog.Portal>
-    </Dialog.Root>
+        <button class="w-full text-center border border-red text-red hover:bg-red/10 active:bg-red/20 transition px-3 py-1 rounded-lg"
+          type="button"
+          onClick={async () => {
+            await resetAppCache();
+            window.location.reload();
+          }}
+        >
+          Effacer le cache
+        </button>
+        <p class="text-[rgb(190,190,190)] text-xs mt-1.5">
+          Supprime le cache stocké par l'application dans le navigateur.
+        </p>
+      </section>
+    </Modal>
   )
 }
--- packages/website/uno.config.ts
+++ packages/website/uno.config.ts
@@ -15,6 +15,13 @@ export default defineConfig({
       tablet: "768px",
       "laptop-sm": "1024px",
       "laptop-lg": "1440px",
+    },
+
+    animation: {
+      keyframes: {
+        "scale-in": "{from{opacity:0;transform:scale(0.96)}to{opacity:1;transform:scale(1)}}",
+        "scale-out": "{from{opacity:1;transform:scale(&)}to{opacity:0;transform:scale(0.96)}}"
+      }
     }
   }
 });
