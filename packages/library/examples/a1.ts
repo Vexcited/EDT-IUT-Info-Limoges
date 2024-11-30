92426b74a34f5e7d47733812bfba83f67b3a3d7f
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-30 13:38:36 +0100

    chore: update example

--- packages/library/examples/a1.ts
+++ packages/library/examples/a1.ts
@@ -1,13 +1,19 @@
-import { getTimetableFromBuffer, YEARS } from "../src";
+import { getLatestTimetableEntry, getTimetableFromBuffer, YEARS } from "../src";
 // @ts-ignore
 import fs from "node:fs";
 
 void async function main () {
   // const entry = await getLatestTimetableEntry(YEARS.A1)
   // const buffer = await entry.getBuffer();
-  // fs.writeFileSync("a1.pdf", buffer);
+  // fs.writeFileSync("a1.pdf", Buffer.from(buffer));
 
   const buffer = await fs.promises.readFile("a1.pdf");
   const timetable = await getTimetableFromBuffer(buffer);
-  console.log("\nTERMINATED", { wn: timetable.header.week_number, l: timetable.lessons.length });
+
+  if (timetable.header.week_number === 13 && timetable.lessons.length === 66) {
+    console.info("\nSUCCESS");
+  }
+  else {
+    console.error("\nFAILED", { wn: `${timetable.header.week_number} vs 13`, l: `${timetable.lessons.length} vs 66` });
+  }
 }()
